1a2ff62fa380dd33db03bdacb3314d7fdd8e8ad3
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-09-09 19:54:21 -0300

    commit

--- LogTest/Program.cs
+++ LogTest/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+
+
+Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+
 Console.WriteLine("Hello, World!");
+
+
+Logger.Debug("debug khjkhj");
+Logger.Info("info jhjhjhjh");
