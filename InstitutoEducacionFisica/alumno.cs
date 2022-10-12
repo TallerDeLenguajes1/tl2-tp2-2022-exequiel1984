e250b39b7afc4647e47aa09d679dfc7d59e104f2
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-10-12 11:56:37 -0300

    Creo clase alumno.cs

---
+++ InstitutoEducacionFisica/alumno.cs
@@ -0,0 +1,31 @@
+namespace InstitutoEducacionFisica
+{
+    public class alumno
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private int dni;
+        private int curso;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Dni { get => dni; set => dni = value; }
+        public int Curso { get => curso; set => curso = value; }
+
+        public alumno()
+        {
+
+        }
+
+        public alumno(int idAlumno, string nombreAlumno, string apellidoAlumno, int dniAlumno, int cursoAlumno)
+        {
+            this.Id = idAlumno;
+            this.Nombre = nombreAlumno;
+            this.Apellido = apellidoAlumno;
+            this.Dni = dniAlumno;
+            this.Curso = cursoAlumno;
+        }
+    }
+}
